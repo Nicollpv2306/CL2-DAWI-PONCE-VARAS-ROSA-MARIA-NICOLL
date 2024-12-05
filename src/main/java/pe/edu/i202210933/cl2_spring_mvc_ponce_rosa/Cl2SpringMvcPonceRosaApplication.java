c2c2c71e2fc43dcd390611ab3b5d511728e3a0d3
Nicollpv2306 <105988853+Nicollpv2306@users.noreply.github.com>
2024-12-05 00:48:30 -0500

    Primer commit - Iniciando CL2

---
+++ src/main/java/pe/edu/i202210933/cl2_spring_mvc_ponce_rosa/Cl2SpringMvcPonceRosaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.i202210933.cl2_spring_mvc_ponce_rosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl2SpringMvcPonceRosaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl2SpringMvcPonceRosaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/i202210933/cl2_spring_mvc_ponce_rosa/Cl2SpringMvcPonceRosaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.i202210933.cl2_spring_mvc_ponce_rosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl2SpringMvcPonceRosaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
